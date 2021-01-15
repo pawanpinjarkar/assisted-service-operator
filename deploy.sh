341aef9d979190e35585ca1804c2b1fe0369bd05
Richard Su <rwsu@redhat.com>
2021-01-15 00:20:38 -0800

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 make bundle-build BUNDLE_IMG=quay.io/rwsu/assisted-service-operator-bundle:0.0.1
-docker push quay.io/rwsu/assisted-service-operator-bundle:0.0.1
+podman push quay.io/rwsu/assisted-service-operator-bundle:0.0.1
 
 opm index add --bundles quay.io/rwsu/assisted-service-operator-bundle:0.0.1 --tag quay.io/rwsu/rwsu-assisted-service-index:0.0.1
 podman push quay.io/rwsu/rwsu-assisted-service-index:0.0.1
