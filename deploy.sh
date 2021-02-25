aa48c84c309262cbd3b28620467224859689ea5e
Pawan Pinjarkar <pawan.pinjarkar@gmail.com>
2021-02-24 22:52:48 -0500

    added placeholders

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-make bundle-build BUNDLE_IMG=quay.io/ppinjark/assisted-service-operator-bundle:0.0.1
-podman push quay.io/ppinjark/assisted-service-operator-bundle:0.0.1
+make bundle-build BUNDLE_IMG=$LOCAL_REGISTRY/$LOCAL_REPOSITORY/assisted-service-operator-bundle:0.0.1
+podman push $LOCAL_REGISTRY/$LOCAL_REPOSITORY/assisted-service-operator-bundle:0.0.1
 
 opm index add \
-    --bundles quay.io/ppinjark/assisted-service-operator-bundle:0.0.1 \
-    --tag quay.io/ppinjark/ppinjark-assisted-service-index:0.0.1 \
+    --bundles $LOCAL_REGISTRY/$LOCAL_REPOSITORY/assisted-service-operator-bundle:0.0.1 \
+    --tag $LOCAL_REGISTRY/$LOCAL_REPOSITORY/ppinjark-assisted-service-index:0.0.1 \
     --pull-tool podman
-    
-podman push quay.io/ppinjark/ppinjark-assisted-service-index:0.0.1
+
+podman push $LOCAL_REGISTRY/$LOCAL_REPOSITORY/ppinjark-assisted-service-index:0.0.1
 
 oc apply -f namespace.yaml
 oc apply -f postgres-pvc.yaml
